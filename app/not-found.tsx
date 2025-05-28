3f0e2190721019eb4a508eade4f27309f707cc2a
bartek <log.mazik.bartek@gmail.com>
2025-05-28 12:53:24 +0200

    404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/Button";
+import { Gliker } from "./fonts/gliker";
+
+const NotFound = () => {
+  return (
+    <div className="flex bg-background rounded-4xl w-full h-[60vh] flex-col gap-8 items-start justify-start p-12 my-12">
+      <h1 className={`${Gliker.className} text-5xl text-primary`}>
+        Strona której szukasz nie została znaleziona{" "}
+      </h1>
+      <Button variant="green" text="POWRÓT DO STRONY GŁÓWNEJ" href="/" />
+    </div>
+  );
+};
+
+export default NotFound;
